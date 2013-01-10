632d72530be42cbedb83832adbc0b8333405a9b7
Cliff Parnitzky <github@cliff-parnitzky.de>
2013-01-10 15:08:03 +0100

    Implemented issue #1
    Also refactord code and added "name" for member and user

--- system/modules/EfgMemberSelectMailer/EfgMemberSelectMailer.php
+++ system/modules/EfgMemberSelectMailer/EfgMemberSelectMailer.php
@@ -27,32 +27,24 @@
  * @license    LGPL
  */
 
-class EfgMemberSelectMailer extends Frontend
-{
+class EfgMemberSelectMailer extends Frontend {
 	/**
 	 * Check if mails should be send and execute it.
 	 */
-	public function processEfgFormData($arrSubmitted, $arrFiles, $intOldId, $arrForm)
-	{
+	public function processEfgFormData($arrSubmitted, $arrFiles, $intOldId, $arrForm) {
 		$config = $this->Database->prepare("SELECT DISTINCT * FROM tl_efgmemberselectmailer WHERE form = ?")
 												 ->execute(intval($arrForm[id]));
-		
-		if ($config->numRows)
-		{
-			if (!$config->form_use_send_mail_check || (strlen($config->form_field_send_mail) > 0 && $arrSubmitted[$config->form_field_send_mail] == $config->form_value_send_mail)) 
-			{
+		if ($config->numRows) {
+			if (!$config->form_use_send_mail_check || (strlen($config->form_field_send_mail) > 0 && $arrSubmitted[$config->form_field_send_mail] == $config->form_value_send_mail)) {
 				$memberIds = $arrSubmitted[$config->form_field_member];
-				if (is_array($memberIds))
-				{
+				if (is_array($memberIds)) {
 					$memberIds = implode(", ", $memberIds);
 				};
 				
-				if (strlen($memberIds) > 0)
-				{
+				if (strlen($memberIds) > 0) {
 					$member = $this->Database->prepare("SELECT * FROM tl_member WHERE id IN (" . $memberIds . ")")
 												 ->execute();
-					while ($member->next())
-					{
+					while ($member->next()) {
 						$this->sendMail($member, $arrForm, $config, $arrSubmitted);
 					}
 				}
@@ -65,11 +57,9 @@ class EfgMemberSelectMailer extends Frontend
 	/**
 	 * Sending the email
 	 */
-	private function sendMail($member, $form, $config, $post)
-	{
+	private function sendMail($member, $form, $config, $post) {
 		// first check if required extension 'ExtendedEmailRegex' is installed
-		if (!in_array('extendedEmailRegex', $this->Config->getActiveModules()))
-		{
+		if (!in_array('extendedEmailRegex', $this->Config->getActiveModules())) {
 			$this->log('EfgMemberSelectMailer: Extension "ExtendedEmailRegex" is required!', 'EfgMemberSelectMailer sendMail()', TL_ERROR);
 			return false;
 		}
@@ -78,46 +68,34 @@ class EfgMemberSelectMailer extends Frontend
 		$objEmail = new Email();
 		$objEmail->logFile = 'EfgMemberSelectMailer.log';
 		$objEmail->from = $config->sender;
-		if (strlen($config->senderName) > 0)
-		{
+		if (strlen($config->senderName) > 0) {
 			$objEmail->fromName = $config->senderName;
 		}
 		
 		$objEmail->subject = $this->replaceEmailInsertTags($config->mailSubject, $member, $form, $config, $post);
 		
-		if ($config->mailTextType == 'text')
-		{
+		if ($config->mailTextType == 'text') {
 			$objEmail->text = $this->replaceEmailInsertTags($config->mailPlainText, $member, $form, $config, $post);
-		}
-		else if ($config->mailTextType == 'html')
-		{
+		} else if ($config->mailTextType == 'html') {
 			$objEmail->html = $this->replaceEmailInsertTags($config->mailHtmlText, $member, $form, $config, $post);
 			$objEmail->text = $this->transformEmailHtmlToText($objEmail->html);
-		}
-		else
-		{
+		} else {
 			$objEmail->text = $this->replaceEmailInsertTags($config->mailPlainText, $member, $form, $config, $post);
 			$objEmail->html = $this->replaceEmailInsertTags($config->mailHtmlText, $member, $form, $config, $post);
 		}
 		
-		try
-		{
+		try {
 			$emailTo = $member->email;
 			
-			if ($GLOBALS['TL_CONFIG']['efgMemberSelectMailerDeveloperMode'])
-			{
+			if ($GLOBALS['TL_CONFIG']['efgMemberSelectMailerDeveloperMode']) {
 				$emailTo = $GLOBALS['TL_CONFIG']['efgMemberSelectMailerDeveloperModeEmail'];
-			}
-			else
-			{
-				if (strlen($config->mailCopy) > 0)
-				{
+			} else {
+				if (strlen($config->mailCopy) > 0) {
 					$emailCC = ExtendedEmailRegex::getEmailsFromList($config->mailCopy);
 					$objEmail->sendCc($emailCC);
 				}
 				
-				if (strlen($config->mailBlindCopy) > 0)
-				{
+				if (strlen($config->mailBlindCopy) > 0) {
 					$emailBCC = ExtendedEmailRegex::getEmailsFromList($config->mailBlindCopy);
 					$objEmail->sendBcc($emailBCC);
 				}
@@ -125,9 +103,7 @@ class EfgMemberSelectMailer extends Frontend
 				$emailTo = $member->email;
 			}
 			return $objEmail->sendTo($emailTo);
-		}
-		catch (Swift_RfcComplianceException $e)
-		{
+		} catch (Swift_RfcComplianceException $e) {
 			$this->log("Mail could not be send: " . $e->getMessage(), "EfgMemberSelectMailer sendMail()", TL_ERROR);
 			return false;
 		}
@@ -135,45 +111,47 @@ class EfgMemberSelectMailer extends Frontend
 	/**
 	 * Replaces all insert tags for the email text.
 	 */
-	private function replaceEmailInsertTags ($text, $member, $form, $config, $post)
-	{
+	private function replaceEmailInsertTags ($text, $member, $form, $config, $post) {
 		$textArray = preg_split('/\{\{([^\}]+)\}\}/', $text, -1, PREG_SPLIT_DELIM_CAPTURE);
 		
-		for ($count = 0; $count < count($textArray); $count++)
-		{
+		for ($count = 0; $count < count($textArray); $count++) {
 			$parts = explode("::", $textArray[$count]);
-			if ($parts[0] == "member")
-			{
-				if ($parts[1] == "password")
-				{
+			if ($parts[0] == "member") {
+				if ($parts[1] == "password") {
 					$textArray[$count] = '';
-				}
-				else if ($parts[1] == "dateOfBirth") {
+				} else if ($parts[1] == "dateOfBirth") {
 					$textArray[$count] = date($GLOBALS['TL_CONFIG']['dateFormat'], $member->$parts[1]);
-				}
-				else if ($parts[1] == "gender") {
+				} else if ($parts[1] == "gender") {
 					$textArray[$count] = $GLOBALS['TL_LANG']['MSC'][$member->$parts[1]];
-				}
-				else
-				{
+				} else if ($parts[1] == "name") {
+					$textArray[$count] = $member->firstname . " " . $member->lastname;
+				} else {
 					$textArray[$count] = $member->$parts[1];
 				}
-			}
-			else if ($parts[0] == "form")
-			{
+			} else if ($parts[0] == "form") {
 				$textArray[$count] = $form[$parts[1]];
-			}
-			else if ($parts[0] == "sender")
-			{
-				switch ($parts[1])
-					{
+			} else if ($parts[0] == "sender") {
+				switch ($parts[1]) {
 						case 'email': $textArray[$count] = $config->sender; break;
 						case 'name': $textArray[$count] = $config->senderName; break;
-					} 
-			}
-			else if ($parts[0] == "post")
-			{
+				} 
+			} else if ($parts[0] == "post") {
 				$textArray[$count] = $post[$parts[1]];
+			} else if ($parts[0] == "user") {
+				if (FE_USER_LOGGED_IN) {
+					$this->import('FrontendUser', 'User');
+					if ($parts[1] == "password") {
+						$textArray[$count] = '';
+					} else if ($parts[1] == "dateOfBirth") {
+						$textArray[$count] = date($GLOBALS['TL_CONFIG']['dateFormat'], $this->User->$parts[1]);
+					} else if ($parts[1] == "gender") {
+						$textArray[$count] = $GLOBALS['TL_LANG']['MSC'][$this->User->$parts[1]];
+					} else if ($parts[1] == "name") {
+						$textArray[$count] = $this->User->firstname . " " . $this->User->lastname;
+					} else {
+						$textArray[$count] = $this->User->$parts[1];
+					}
+				} 
 			}
 		}
 		
@@ -184,8 +162,7 @@ class EfgMemberSelectMailer extends Frontend
 	/**
 	 * Creates the text from the html for the email.
 	 */
-	private function transformEmailHtmlToText ($emailHtml)
-	{
+	private function transformEmailHtmlToText ($emailHtml) {
 		$emailText = $emailHtml;
 		$emailText = str_replace("</p> ", "\n\n", $emailText);
 		$emailText = str_replace("</ul> ", "\n", $emailText);
